d71a6ffe003fe12d61d271d1854693a1cc571434
K-Alex-N <kurochkinalexei@yandex.ru>
2023-02-10 21:59:49 +0300

    4ое видео

--- app/crm/views.py
+++ app/crm/views.py
@@ -1,40 +1,57 @@
 import uuid
-from typing import Any
+# from typing import Any
 from aiohttp.web_exceptions import HTTPNotFound
 # from aiohttp.web_response import json_response
-from aiohttp.web_response import Response
+# from aiohttp.web_response import Response
+from aiohttp_apispec import docs, request_schema, response_schema
 
 from app.crm.models import User
+from app.crm.schemes import UserSchema, ListUsersResponseSchema, UserGetRequestSchema, UserGetResponseSchema, \
+    UserAddSchema
 from app.web.app import View
-from aiohttp.web import json_response as aiohttp_json_response
-def json_response(data: Any = None, status: str = 'ok') -> Response:
-    if data is None:
-        data = {}
-    return aiohttp_json_response(
-        data={
-            "status": status,
-            "data": data,
-        })
+
+
+from app.web.schemes import OkResponseSchema
+from app.web.utils import json_response
+
+
+# from aiohttp.web import json_response as aiohttp_json_response
+# def json_response(data: Any = None, status: str = 'ok') -> Response:
+#     if data is None:
+#         data = {}
+#     return aiohttp_json_response(
+#         data={
+#             "status": status,
+#             "data": data,
+#         })
 
 class AddUserView(View):
+    @docs(tags=['crm'], summary='add new user', description='Test method description')
+    @request_schema(UserAddSchema)
+    @response_schema(OkResponseSchema, 200)
     async def post(self):
-        data = await self.request.json()
+        data = self.request['data']
         user = User(email=data["email"], id_=uuid.uuid4())
         await self.request.app.crm_accessor.add_user(user)
         return json_response()
 
 
 class ListUsersView(View):
+    @docs(tags=['crm'], summary='list users', description='Test measdadthod description')
+    @response_schema(ListUsersResponseSchema, 200)
     async def get(self):
         users = await self.request.app.crm_accessor.list_users()
         raw_users = [{'email': user.email, "id": str(user.id_)} for user in users]
-        return json_response(data={'status': 'ok', 'users': raw_users})
+        return json_response(data={'users': raw_users})
 
 class GetUserView(View):
+    @docs(tags=['crm'], summary='get user', description='Test method description')
+    @request_schema(UserGetRequestSchema)
+    @response_schema(UserGetResponseSchema, 200)
     async def get(self):
         user_id = self.request.query['id']
         user = await self.request.app.crm_accessor.get_user(uuid.UUID(user_id))
         if user:
-            return json_response(data={'status': 'ok', 'user': {'email': user.email, 'id': str(user.id_)}})
+            return json_response(data={'user': {'email': user.email, 'id': str(user.id_)}})
         else:
             raise HTTPNotFound
