0479863d159cd6375efe2b91b99fbd66126c19c6
K-Alex-N <kurochkinalexei@yandex.ru>
2023-02-10 23:42:08 +0300

    4ое видео

--- app/crm/views.py
+++ app/crm/views.py
@@ -3,7 +3,7 @@ import uuid
 from aiohttp.web_exceptions import HTTPNotFound
 # from aiohttp.web_response import json_response
 # from aiohttp.web_response import Response
-from aiohttp_apispec import docs, request_schema, response_schema
+from aiohttp_apispec import docs, request_schema, response_schema, querystring_schema
 
 from app.crm.models import User
 from app.crm.schemes import UserSchema, ListUsersResponseSchema, UserGetRequestSchema, UserGetResponseSchema, \
@@ -42,11 +42,13 @@ class ListUsersView(View):
     async def get(self):
         users = await self.request.app.crm_accessor.list_users()
         raw_users = [{'email': user.email, "id": str(user.id_)} for user in users]
+        # raw_users = [UserSchema().dump(user) for user in users]
         return json_response(data={'users': raw_users})
 
 class GetUserView(View):
     @docs(tags=['crm'], summary='get user', description='Test method description')
-    @request_schema(UserGetRequestSchema)
+    # @request_schema(UserGetRequestSchema)
+    @querystring_schema(UserGetRequestSchema)
     @response_schema(UserGetResponseSchema, 200)
     async def get(self):
         user_id = self.request.query['id']
